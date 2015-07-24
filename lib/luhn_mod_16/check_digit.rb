417da3267db75deb9b1f74f13fff9e80adc72476
Luke Farmer <luke.e.farmer@gmail.com>
2015-07-24 10:26:40 +0100

    Refactor check_digit, fix last step, add spec for odd case

--- lib/luhn_mod_16/check_digit.rb
+++ lib/luhn_mod_16/check_digit.rb
@@ -1,52 +1,30 @@
 module LuhnMod16
   class CheckDigit
-    attr_accessor :string
+    attr_accessor :input
 
     def initialize(string)
-      self.string = string
+      self.input = string
     end
 
     def check_digit
-      (16 - sum_reduced_characters) % 16
+      array = (code_point_map @input).reverse
+      array.map!.with_index do |int,i|
+        next int unless i.even?
+        doubled_hex = (int * 2).to_s(16)
+        next int unless doubled_hex.index(/[^0-9]/).nil?
+        doubled_hex.chars.sum &:to_i
+      end
+      reverse_code_point_map(16 - array.sum % 16)
     end
 
     private
 
-    def code_mapped
-      self.string.chars.map { |character| character.hex }
+    def code_point_map string
+      string.chars.map &:hex
     end
 
-    def double_alternate_characters
-      mapped = code_mapped.reverse
-      (0..mapped.length - 1).step(2).map do |index|
-        mapped_character = mapped[index]
-        next unless mapped_character > 0
-
-        converted_character = (mapped_character * 2).to_i.to_s(16)
-        mapped[index] = converted_character if converted_character.to_i != 0
-      end
-      mapped.reverse.map &:to_s
-    end
-
-    def reduced_doubled_characters
-      mapped = double_alternate_characters.reverse
-      (0..mapped.length - 1).step(2).map do |index|
-        mapped[index] = mapped[index].split('').map(&:to_i).sum
-      end
-
-      mapped.reverse
-    end
-
-    def sum_reduced_characters
-      reduced_doubled_characters.map(&:to_i).sum
-    end
-
-    def reversed_string
-      string.reverse
-    end
-
-    def split_string
-      self.string.chars
+    def reverse_code_point_map int
+      int.to_s(16).upcase
     end
   end
 end
--- spec/luhn_mod_16/check_digit_spec.rb
+++ spec/luhn_mod_16/check_digit_spec.rb
@@ -4,12 +4,15 @@ describe LuhnMod16::CheckDigit do
 
   subject { LuhnMod16::CheckDigit.new(string).check_digit }
 
-  context 'when the string is valid' do
+  context 'Royal Mail documentation example' do
     let(:string) { '0B012722900021AC35B2' }
 
+    it { is_expected.to eq '1' }
+  end
 
-    it 'should produce the correct check digit' do
-      expect(subject).to eq 1
-    end
+  context 'Ambiguous case not covered by docs' do
+    let(:string) { '0B012722900021AC35BD' }
+
+    it { is_expected.to eq '8' }
   end
 end
